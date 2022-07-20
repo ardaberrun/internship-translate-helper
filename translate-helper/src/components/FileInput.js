72e9ae13fc489241c0a2558b0c1c61d4e8c3d333
ardaberrun <ardaberrun@gmail.com>
2022-07-20 11:08:33 +0300

    #6957 add file inputs

---
+++ translate-helper/src/components/FileInput.js
@@ -0,0 +1,15 @@
+import { ReactComponent as FileUploadLogo } from '../assets/upload.svg';
+
+function FileInput({ file, title }) {
+  return (
+    <div className="file-input">
+      <input type="file" name={file} id={file} className="file-input__input" />
+      <label for={file}>
+        <FileUploadLogo />
+        {title}
+      </label>
+    </div>
+  );
+}
+
+export default FileInput;
