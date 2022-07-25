6f73f759c55084e9c6a78958b45b11b859d53af2
ardaberrun <ardaberrun@gmail.com>
2022-07-25 14:13:18 +0300

    #7028 refactor styles

--- translate-helper/src/components/Translation.js
+++ translate-helper/src/components/Translation.js
@@ -3,15 +3,19 @@ import FileUploader from './FileUploader';
 
 function Translation({ file, fileType, handleChange, changes }) {
   const applyClass = (key) => {
+    if (changes.removed.includes(key)) {
+      return 'changes changes-removed';
+    }
+
     if (changes.modified.includes(key)) {
-      return 'changes-modify';
+      return 'changes changes-modify';
     }
 
     if (changes.added.includes(key)) {
-      return 'changes-add';
+      return 'changes changes-add';
     }
 
-    return '';
+    return 'changes';
   };
 
   return !Object.keys(file).length ? (
